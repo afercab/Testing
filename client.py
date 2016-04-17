32284341ee0792aa4f8f0971ba53b32611e7e6c7
Ángel Fernández Cabezas <jmkaos@gmail.com>
2016-04-17 18:49:47 +0200

    Upgrade client code by refactoring it and using clean code
    Code refactored.
    Code clean directives applied.
    Formatted according to Python's style guide.

--- client.py
+++ client.py
@@ -1,23 +1,41 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("127.0.0.1", 4009))
 
+def create_and_bind_socket():
+    global connection
+    host = "127.0.0.1"
+    port = 4009
+    try:
+        connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connection.bind((host, port))
+    except ValueError:
+        raise ValueError("Could not open a connection.")
+
+
+def successful_try():
+    print "Nice Guess!!!"
+    attempts = connection.recv(80)
+    print attempts, "attempts were needed"
+    connection.close()
+
+
+def wrong_guess():
+    global guess
+    guess = (int(raw_input("Make another guess...")))
+    connection.send(guess)
+    print connection.recv(80)
+
+
+create_and_bind_socket()
 print "Welcome to the Number-Guessing game!!!!"
 print "Please type a number between 0 and 10`. If you find it then you win! So..."
 
 while True:
     guess = raw_input("What's the Number I am looking for?")
 
-    s.send(guess)
-    serverAnswer = s.recv(80)
-    if serverAnswer == "No":        
-        guess = (int(raw_input("Make another guess...")))
-        s.send(guess)
-        print s.recv(80)
+    connection.send(guess)
+    serverAnswer = connection.recv(80)
+    if serverAnswer == "No":
+        wrong_guess()
     if serverAnswer == "Yes":
-        print "Nice Guess!!!"
-        attempts = s.recv(80)
-        print attempts, "attempts were needed"
-        s.close()
-        
+        successful_try()
